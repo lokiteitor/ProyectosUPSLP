45ccf99884024138e8b6d866521b442646e0166a
lokiteitor <lokiteitor513@gmail.com>
2016-04-19 20:47:27 -0500

    https en nodejs

--- StarMedica/server/app.js
+++ StarMedica/server/app.js
@@ -1,17 +1,20 @@
 var express = require('express');
-var http = require('http');
+var https = require('https');
 var app = express();
-var server = http.createServer(app);
+var fs = require('fs');
+var server = https.createServer({
+	key: fs.readFileSync('privkey.pem'),
+	cert: fs.readFileSync('cert.pem')
+},
+app);
 
 // creamos el servidor
 server.listen(1337);
 var io = require("socket.io").listen(server)
 
-io.sockets.on("connection",function (data) {
-    console.log('conectado');
-    io.sockets.on("newFrame",function (data) {
-        console.log(data);
-        io.sockets.broadcast.emit("setFrame",data);    
+io.sockets.on("connection",function (socket) {
+    socket.on("newFrame",function (data) {
+        socket.broadcast.emit("setFrame",data);    
     })
 });
 
