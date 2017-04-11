637c6c3f0a2acc9fe0e7b1ccd633585202e6f4d9
lokiteitor <lokiteitor513@gmail.com>
2017-04-11 13:02:12 -0500

    Creacion de la base de las interfacez graficas

---
+++ CallCenterClient/src/main/java/mx/edu/upslp/callcenterclient/main/Main.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mx.edu.upslp.callcenterclient.main;
+
+/**
+ *
+ * @author David Delgado Hernandez 150205@upslp.edu.mx
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
